a40236704d424cd39724d3bf280eb9a0c95632fd
vanssea <cristinvanessa6@gmail.com>
2026-02-24 22:30:00 +0700

    Langkah 7 – Navigasi Imperatif (router.push)

--- P3/my-app/src/pages/auth/login.tsx
+++ P3/my-app/src/pages/auth/login.tsx
@@ -1,12 +1,22 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const halamanLogin = () => {
+  const { push } = useRouter();
+  const handlerLogin = () => {
+    // logic login disini
+    push('/produk');
+  }
+
   return (
     <div>
       <h1>Halaman Login</h1>
+      <button onClick={handlerLogin}>Login</button> <br />
+      <button onClick={() => push('/produk')}>Login</button> <br />
+      <button onClick={() => handlerLogin()}>Login</button> <br />
       <Link href="/auth/register">Ke Halaman Register</Link>
     </div>
   );
-};
+}
 
 export default halamanLogin;
--- P3/my-app/src/pages/produk/index.tsx
+++ P3/my-app/src/pages/produk/index.tsx
@@ -1,10 +1,14 @@
-const produk = () => {
-  return (
-    <div>
-      Produk User Page
-    </div>
-  );
-};
+import { useRouter } from "next/router"; 
+import { useEffect, useState } from "react"; 
 
+const produk = () => { 
+  // const [isLogin, setIsLogin] = useState(false); 
+  // const { push } = useRouter(); 
+  // useEffect(() => { // if (!isLogin) { 
+  //   // push("/auth/login"); 
+  // } // }, []); 
+  return ( 
+  <div>Produk User Page</div> 
+); 
+}; 
 export default produk;
-
