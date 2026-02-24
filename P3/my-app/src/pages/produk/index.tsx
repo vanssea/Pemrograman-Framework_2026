d193d1df605b7d9b3a0a8b809adbaa497ec1eb63
vanssea <cristinvanessa6@gmail.com>
2026-02-24 22:34:01 +0700

    Langkah 8 – Simulasi Redirect (Belum Login)

--- P3/my-app/src/pages/produk/index.tsx
+++ P3/my-app/src/pages/produk/index.tsx
@@ -1,14 +1,17 @@
-import { useRouter } from "next/router"; 
-import { useEffect, useState } from "react"; 
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
-const produk = () => { 
-  // const [isLogin, setIsLogin] = useState(false); 
-  // const { push } = useRouter(); 
-  // useEffect(() => { // if (!isLogin) { 
-  //   // push("/auth/login"); 
-  // } // }, []); 
-  return ( 
-  <div>Produk User Page</div> 
-); 
-}; 
+const produk = () => {
+  const [isLogin, setIsLogin] = useState(false);
+  const { push } = useRouter();
+  useEffect(() => {
+    if (!isLogin) {
+      push("/auth/login");
+    }
+  }, []);
+  return (
+    <div>Produk User Page</div>
+  );
+};
+
 export default produk;
