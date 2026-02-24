f38cc23691af3b5c5b4ea52d1b90b05aad42698f
vanssea <cristinvanessa6@gmail.com>
2026-02-24 22:10:40 +0700

    Langkah 5 – Validasi Parameter

--- P3/my-app/src/pages/shop/[[...slug]].tsx
+++ P3/my-app/src/pages/shop/[[...slug]].tsx
@@ -1,10 +1,9 @@
-
 import { useRouter } from "next/router";
 
 const halamanToko = () => {
-  // const Router = useRouter();
-  // console.log(Router);
+
   const { query } = useRouter();
+  const slug = query.slug as string[] | undefined;
 
   return (
     <div>
@@ -12,6 +11,9 @@ const halamanToko = () => {
       {/* <p>Toko: {`${query.slug && query.slug[0]+"-"+ query.slug[1]}`}</p> menggunakan backtick bukan petik satu */}
       <p>
         Toko: {Array.isArray(query.slug) ? query.slug.join("-") : query.slug}
+      </p>
+      <p>
+        Kategori: {slug ? slug[0] : "Semua Kategori"}
       </p>
     </div>
   );
