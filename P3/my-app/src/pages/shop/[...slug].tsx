9506245ffe8ef4512e083305db56adf9a529d6a3
vanssea <cristinvanessa6@gmail.com>
2026-02-24 21:41:03 +0700

    Langkah 3 – Pengujian Catch-All Route

--- P3/my-app/src/pages/shop/[...slug].tsx
+++ P3/my-app/src/pages/shop/[...slug].tsx
@@ -1,16 +1,20 @@
+
 import { useRouter } from "next/router";
 
 const halamanToko = () => {
   // const Router = useRouter();
-  //console.log(Router);
+  // console.log(Router);
   const { query } = useRouter();
+
   return (
     <div>
       <h1>Halaman Toko</h1>
-      <p>Toko: {`${query.slug && query.slug[0]+"-"+ query.slug[1]}`}</p> {/* menggunakan backtick bukan petik satu*/}
+      {/* <p>Toko: {`${query.slug && query.slug[0]+"-"+ query.slug[1]}`}</p> menggunakan backtick bukan petik satu */}
+      <p>
+        Toko: {Array.isArray(query.slug) ? query.slug.join("-") : query.slug}
+      </p>
     </div>
   );
 };
 
 export default halamanToko;
-
