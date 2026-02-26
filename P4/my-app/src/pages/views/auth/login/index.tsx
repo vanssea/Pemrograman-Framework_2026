7060660b7363ee92e73b2a6c5c7dc0e585eaaaaf
vanssea <cristinvanessa6@gmail.com>
2026-02-26 23:29:31 +0700

    Langkah 6 - Inline Styling (CSS-in-JS)

--- P4/my-app/src/pages/views/auth/login/index.tsx
+++ P4/my-app/src/pages/views/auth/login/index.tsx
@@ -14,6 +14,14 @@ const TampilanLogin = () => {
     <div className={styles.login}>
       <h1>Halaman Login</h1>
       <button onClick={() => handleLogin()}>Login</button> <br />
+      <h1 style={{
+        color: "red",
+        border: "1px solid red",
+        borderRadius: "5px",
+        padding: "5px"
+      }}>
+        belum punya akun
+      </h1>
       <Link href="/auth/register">Ke Halaman Register</Link>
     </div>
   );
