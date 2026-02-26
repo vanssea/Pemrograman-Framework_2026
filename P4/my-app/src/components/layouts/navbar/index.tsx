f98986e4f8eb9250dd1d4a07a1d608656cecaaf4
vanssea <cristinvanessa6@gmail.com>
2026-02-26 22:24:36 +0700

    Langkah 2 - CSS Module (Local Scope)

--- P4/my-app/src/components/layouts/navbar/index.tsx
+++ P4/my-app/src/components/layouts/navbar/index.tsx
@@ -1,7 +1,11 @@
+import styles from './navbar.module.css';
+
 const Navbar = () => {
   return (
-    <div className="navbar">
-      <div>Navbar Component </div>
+    <div className={styles.navbar}>
+      <div>
+        <h1>Navbar Component</h1>
+      </div>
     </div>
   );
 };
