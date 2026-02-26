67aaf78050257e9e01d2ff26f946e207909ca5d8
vanssea <cristinvanessa6@gmail.com>
2026-02-26 22:31:10 +0700

    Langkah 3 - Styling untuk Pages (CSS Module)

--- P4/my-app/src/pages/auth/login.tsx
+++ P4/my-app/src/pages/auth/login.tsx
@@ -1,21 +1,22 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from './login.module.css';
 
 const halamanLogin = () => {
   const { push } = useRouter();
+
   const handlerLogin = () => {
-    // logic login disini
-    localStorage.setItem('isLogin', 'true');
+    // logika login disini
     push('/produk');
-  }
+  };
 
   return (
-    <div>
+    <div className={styles.login}>
       <h1>Halaman Login</h1>
-      <button onClick={handlerLogin}>Login</button> <br />
+      <button onClick={() => handlerLogin()}>Login</button> <br />
       <Link href="/auth/register">Ke Halaman Register</Link>
     </div>
   );
-}
+};
 
 export default halamanLogin;
