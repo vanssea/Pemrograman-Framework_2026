c7fdf048615423b0ece5f02409aee6b6a73b76bf
vanssea <cristinvanessa6@gmail.com>
2026-03-01 22:35:07 +0700

    Tugas Praktikum - Tugas 1

--- P5/my-app/src/pages/404.tsx
+++ P5/my-app/src/pages/404.tsx
@@ -1,13 +1,30 @@
-import styles from "@/styles/404.module.scss";
+import Head from "next/head";
+import styles from "../styles/404.module.scss";
 
-const Custom404 = () => {
+export default function Custom404() {
   return (
-    <div className={styles.error}>
-      <img src="/page-not-found.png" alt="404" className={styles.error__image} />
-      <h1>404 - Halaman Tidak Ditemukan</h1>
-      <p>Maaf, halaman yang Anda cari tidak ada.</p>
-    </div>
-  );
-};
+    <>
+      <Head>
+        <title>404 - Halaman Tidak Ditemukan</title>
+        <meta
+          name="description"
+          content="Maaf, halaman yang kamu cari tidak tersedia."
+        />
+      </Head>
 
-export default Custom404;
+      <div className={styles.error}>
+        <img
+          src="/page-not-found.png"
+          alt="404"
+          className={styles.error__image}
+        />
+
+        <h1 className={styles.error__title}>Oops! 404</h1>
+
+        <p className={styles.error__desc}>
+          Halaman yang kamu cari tidak ditemukan atau sudah dipindahkan.
+        </p>
+      </div>
+    </>
+  );
+}
