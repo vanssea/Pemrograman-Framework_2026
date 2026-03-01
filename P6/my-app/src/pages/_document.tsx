31035b1a962bfeb871b4eff20ac7082ee196ed29
vanssea <cristinvanessa6@gmail.com>
2026-03-01 23:26:43 +0700

    Jobsheet 7

---
+++ P6/my-app/src/pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <html lang="id">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </html>
+  )
+}
