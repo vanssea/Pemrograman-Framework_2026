3309476dab28197cb9366462c69bd528b1b876fe
vanssea <cristinvanessa6@gmail.com>
2026-03-01 21:21:51 +0700

    Langkah 2 – Membuat Custom Document

--- P5/my-app/src/pages/_document.tsx
+++ P5/my-app/src/pages/_document.tsx
@@ -2,12 +2,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <html lang="id">
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
-    </Html>
+    </html>
   )
 }
