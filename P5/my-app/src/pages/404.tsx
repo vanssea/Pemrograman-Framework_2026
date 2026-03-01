3df0ea4f746ff510be3a9785419ef7aebfa8975a
vanssea <cristinvanessa6@gmail.com>
2026-03-01 22:01:16 +0700

    Langkah 4 – Membuat Custom Error Page (404)

---
+++ P5/my-app/src/pages/404.tsx
@@ -0,0 +1,10 @@
+const Custom404 = () => {
+  return (
+    <div>
+      <h1>404 - Halaman Tidak Ditemukan</h1>
+      <p>Maaf, halaman yang Anda cari tidak ditemukan.</p>
+    </div>
+    );
+};
+
+export default Custom404;
