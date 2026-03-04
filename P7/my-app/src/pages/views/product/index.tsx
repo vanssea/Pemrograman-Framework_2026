de7bdd342230cc2c086a8a827e3d56eccd8dc940
vanssea <cristinvanessa6@gmail.com>
2026-03-04 13:51:04 +0700

    Bagian 3 – Implementasi Skeleton Loading (modifikasi index.tsx & product.module.scss)

--- P7/my-app/src/pages/views/product/index.tsx
+++ P7/my-app/src/pages/views/product/index.tsx
@@ -3,8 +3,8 @@ import styles from "../../produk/product.module.scss";
 type ProductType = {
   id: string;
   name: string;
-  price: number;
   image: string;
+  price: number;
   category: string;
 };
 
@@ -12,29 +12,29 @@ const TampilanProduk = ({ products }: { products: ProductType[] }) => {
   return (
     <div className={styles.produk}>
       <h1 className={styles.produk__title}>Daftar Produk</h1>
-
       <div className={styles.produk__content}>
-        {products.map((product: ProductType) => (
-          <div key={product.id} className={styles.produk__item}>
-            
-            <div className={styles.produk__image}>
-              <img src={product.image} alt={product.name} />
+        {/* products.map((products: ProductType) => (
+          <div key={products.id} className={styles.produk__content__item}>
+            <div className={styles.produk__content__item__image}>
+              <img src={products.image} alt={products.name} width={200} />
             </div>
-
-            <h4 className={styles.produk__name}>
-              {product.name}
+            <h4 className={styles.produk__content__item__name}>
+              {products.name}
             </h4>
-
-            <p className={styles.produk__category}>
-              {product.category}
+            <p className={styles.produk__content__item__category}>
+              {products.category}
             </p>
-
-            <p className={styles.produk__price}>
-              Rp {product.price.toLocaleString("id-ID")}
+            <p className={styles.produk__content__item__price}>
+              Rp {products.price.toLocaleString()}
             </p>
-
           </div>
-        ))}
+        )) */}
+        <div className={styles.produk__content__skeleton}>
+          <div className={styles.produk__content__skeleton__image}></div>
+          <div className={styles.produk__content__skeleton__name}></div>
+          <div className={styles.produk__content__skeleton__category}></div>
+          <div className={styles.produk__content__skeleton__price}></div>
+        </div>
       </div>
     </div>
   );
