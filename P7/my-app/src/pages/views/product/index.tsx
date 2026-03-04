563971f375f7040dfb4f812b53801980da5e2f96
vanssea <cristinvanessa6@gmail.com>
2026-03-04 14:25:39 +0700

    Bagian 3 – Implementasi Skeleton Loading (modifikasi agar skeletn muncul sesuai jumlah produknya

--- P7/my-app/src/pages/views/product/index.tsx
+++ P7/my-app/src/pages/views/product/index.tsx
@@ -3,8 +3,8 @@ import styles from "../../produk/product.module.scss";
 type ProductType = {
   id: string;
   name: string;
-  image: string;
   price: number;
+  image: string;
   category: string;
 };
 
@@ -13,28 +13,37 @@ const TampilanProduk = ({ products }: { products: ProductType[] }) => {
     <div className={styles.produk}>
       <h1 className={styles.produk__title}>Daftar Produk</h1>
       <div className={styles.produk__content}>
-        {/* products.map((products: ProductType) => (
-          <div key={products.id} className={styles.produk__content__item}>
-            <div className={styles.produk__content__item__image}>
-              <img src={products.image} alt={products.name} width={200} />
+        {products.length > 0 ? (
+          products.map((products: ProductType) => (
+            <div key={products.id} className={styles.produk__content__item}>
+              <img 
+                src={products.image} 
+                alt={products.name} 
+                className={styles.produk__content__item__image}
+              />
+              <div className={styles.produk__content__item__name}>
+                {products.name}
+              </div>
+              <div className={styles.produk__content__item__category}>
+                {products.category}
+              </div>
+              <div className={styles.produk__content__item__price}>
+                Rp {products.price.toLocaleString()}
+              </div>
             </div>
-            <h4 className={styles.produk__content__item__name}>
-              {products.name}
-            </h4>
-            <p className={styles.produk__content__item__category}>
-              {products.category}
-            </p>
-            <p className={styles.produk__content__item__price}>
-              Rp {products.price.toLocaleString()}
-            </p>
-          </div>
-        )) */}
-        <div className={styles.produk__content__skeleton}>
-          <div className={styles.produk__content__skeleton__image}></div>
-          <div className={styles.produk__content__skeleton__name}></div>
-          <div className={styles.produk__content__skeleton__category}></div>
-          <div className={styles.produk__content__skeleton__price}></div>
-        </div>
+          ))
+        ) : (
+          <>
+            {[1, 2, 3, 4].map((i) => (
+              <div key={i} className={styles.produk__content__item}>
+                <div className={styles.produk__content__skeleton__image}></div>
+                <div className={styles.produk__content__skeleton__name}></div>
+                <div className={styles.produk__content__skeleton__category}></div>
+                <div className={styles.produk__content__skeleton__price}></div>
+              </div>
+            ))}
+          </>
+        )}
       </div>
     </div>
   );
