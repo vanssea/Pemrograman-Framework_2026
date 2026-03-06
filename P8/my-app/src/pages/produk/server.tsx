030cbb2a6a05e0c1a42a04b8742d4a353435fc11
vanssea <cristinvanessa6@gmail.com>
2026-03-06 16:17:48 +0700

    Bagian 3 – Refactor Type ( produk type )

--- P8/my-app/src/pages/produk/server.tsx
+++ P8/my-app/src/pages/produk/server.tsx
@@ -1,12 +1,5 @@
 import TampilanProduk from "../views/product";
-
-type ProductType = {
-  id: string;
-  name: string;
-  price: number;
-  image: string;
-  category: string;
-};
+import { ProductType } from "../types/Product.type";
 
 const halamanProdukServer = (props:{products:ProductType[]}) => {
   const { products } = props;
@@ -18,16 +11,19 @@ const halamanProdukServer = (props:{products:ProductType[]}) => {
   );
 };
 
+
+
 export default halamanProdukServer;
 
-// Fungsi getServerSideProps akan dipanggil setiap kali halaman ini diakses, dan akan mengambil data produk dari API sebelum merender halaman.
+
+// Fungsi getServerSideProps akan dipanggil setiap kali halaman ini diakses, dan akan mengambil data produk dari API sebelum merender halaman
 export async function getServerSideProps() {
-  const res = await fetch("http://localhost:3000/api/produk");
-  const response = await res.json();
-  // console.log("Data produk yang diambil dari API:", response);
-  return {
-    props: {
-      products: response.data, // Pastikan untuk memberikan nilai default jika data tidak tersedia
-    },
-  };
+    const res = await fetch("http://localhost:3000/api/produk");
+    const response = await res.json();
+    // console.log("Data produk yang diambil dari API:", response);
+    return {
+        props: {
+            products: response.data, // Pastikan untuk memberikan nilai default jika data tidak tersedia
+        },
+    };
 }
