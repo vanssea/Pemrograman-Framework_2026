f10e5435dc6af09d8b66349e1fa4a26a6d5f2573
vanssea <cristinvanessa6@gmail.com>
2026-03-31 15:08:19 +0700

    Bagian 1 – Membuat Dynamic Route

--- P10/my-app/src/views/product/index.tsx
+++ P10/my-app/src/views/product/index.tsx
@@ -1,5 +1,5 @@
 import styles from "../produk/product.module.scss";
-
+import Link from "next/link";
 type ProductType = {
   id: string;
   name: string;
@@ -8,49 +8,45 @@ type ProductType = {
   category: string;
 };
 
-const TampilanProduk = ({ products }: { products: ProductType[] }) => {
+const TampilanProduk = ({ products = [] }: { products?: ProductType[] }) => {
   return (
     <div className={styles.produk}>
       <h1 className={styles.produk__title}>Daftar Produk</h1>
       <div className={styles.produk__content}>
         {products.length > 0 ? (
-          products.map((products: ProductType, index: number) => (
-            <div 
-              key={products.id} 
-              className={styles.produk__content__item}
-              style={{ animationDelay: `${index * 0.1}s` }}
-            >
-              <img 
-                src={products.image} 
-                alt={products.name} 
-                className={styles.produk__content__item__image}
-              />
-              <div className={styles.produk__content__item__name}>
-                {products.name}
-              </div>
-              <div className={styles.produk__content__item__category}>
-                {products.category}
-              </div>
-              <div className={styles.produk__content__item__price}>
-                Rp {products.price.toLocaleString("id-ID")}
-              </div>
-            </div>
-          ))
-        ) : (
           <>
-            {[1, 2, 3, 4, 5, 6, 7, 8].map((i) => (
-              <div key={i} className={styles.produk__content__item}>
-                <div className={styles.produk__content__skeleton__image}></div>
-                <div className={styles.produk__content__skeleton__name}></div>
-                <div className={styles.produk__content__skeleton__category}></div>
-                <div className={styles.produk__content__skeleton__price}></div>
-              </div>
+            {products.map((product: ProductType) => (
+              <Link
+                href={`/produk/${product.id}`}
+                key={product.id}
+                className={styles.produk__content__item}
+              >
+                <div className={styles.produk__content__item__image}>
+                  <img src={product.image} alt={product.name} width={200} />
+                </div>
+                <h4 className={styles.produk__content__item__name}>
+                  {product.name}
+                </h4>
+                <p className={styles.produk__content__item__category}>
+                  {product.category}
+                </p>
+                <p className={styles.produk__content__item__price}>
+                  Rp {product.price.toLocaleString("id-ID")}
+                </p>
+              </Link>
             ))}
           </>
+        ) : (
+          <div className={styles.produk__content__skeleton}>
+            <div className={styles.produk__content__skeleton__image}></div>
+            <div className={styles.produk__content__skeleton__name}></div>
+            <div className={styles.produk__content__skeleton__category}></div>
+            <div className={styles.produk__content__skeleton__price}></div>
+          </div>
         )}
       </div>
     </div>
   );
-};
+}
 
 export default TampilanProduk;
