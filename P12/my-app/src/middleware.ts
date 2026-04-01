83f271ff9863af8ca95dfe7e5c3443f765065550
vanssea <cristinvanessa6@gmail.com>
2026-04-01 16:26:09 +0700

    Bagian 5 – Simulasi Sistem Login

--- P12/my-app/src/middleware.ts
+++ P12/my-app/src/middleware.ts
@@ -2,8 +2,14 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/", request.url));
+  const isLogin = false;
+  if (isLogin) {
+    return NextResponse.next();
+  } else {
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
 
+  //return NextResponse.redirect(new URL("/", request.url));
   //return NextResponse.next();
 }
 
