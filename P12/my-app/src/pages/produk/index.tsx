ad8c7a0dc24fded0382b3effcfbc11064050d243
vanssea <cristinvanessa6@gmail.com>
2026-04-01 16:11:44 +0700

    Bagian 1 & 2 – Struktur Dasar Middleware

--- P12/my-app/src/pages/produk/index.tsx
+++ P12/my-app/src/pages/produk/index.tsx
@@ -4,22 +4,21 @@ import TampilanProduk from "../../views/product";
 import useSWR from "swr";
 import fetcher from "../../utils/swr/fetcher";
 
-
-
-// const fetcher = (url: string) => fetch(url).then((res) => res.json());
 const kategori = () => {
   // const [isLogin, setIsLogin] = useState(true);
   const { push } = useRouter();
   const [products, setProducts] = useState([]);
-  // console.log("products:", products);...
 
   const { data, error, isLoading } = useSWR("/api/produk", fetcher);
-  //cek apakah data, error, dan isLoading sudah benar...
 
+  useEffect(() => {
+    if (error) push("/404");
+    if (data?.data) setProducts(data.data);
+  }, [data, error, push]);
 
   return (
     <div>
-      <TampilanProduk products={isLoading ? [] : data.data} />
+      <TampilanProduk products={isLoading ? [] : products} />
     </div>
   );
 };
