902dad50cf963114d1bd0050ac7ff3fccb62eafd
vanssea <cristinvanessa6@gmail.com>
2026-04-03 19:26:00 +0700

    Bagian 2 –  Membuat API Register

--- P14/my-app/src/utils/db/servicefirebase.ts
+++ P14/my-app/src/utils/db/servicefirebase.ts
@@ -1,34 +1,64 @@
-import { getFirestore, collection, getDocs, getDoc, doc } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  Firestore,
+  getDoc,
+  doc,
+  query,
+  addDoc,
+  where,
+} from "firebase/firestore";
 import app from "./firebase";
 
 const db = getFirestore(app);
 
-export async function getData(collectionName: string) {
+export async function retrieveProducts(collectionName: string) {
   const snapshot = await getDocs(collection(db, collectionName));
-
-  const data = snapshot.docs.map((docItem) => ({
-    id: docItem.id,
-    ...docItem.data(),
+  const data = snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
   }));
-
   return data;
 }
 
 export async function retrieveDataByID(collectionName: string, id: string) {
-  try {
-    const docRef = doc(db, collectionName, id);
-    const snapshot = await getDoc(docRef);
+  const snapshot = await getDoc(doc(db, collectionName, id));
+  const data = snapshot.data();
+  return data;
+}
 
-    if (!snapshot.exists()) {
-      return null;
-    }
+export async function signUp(
+  userData: {
+    email: string;
+    fullname: string;
+    password: string;
+  },
+  callback: Function,
+) {
+  const q = query(
+    collection(db, "users"),
+    where("email", "==", userData.email),
+  );
+  const querySnapshot = await getDocs(q);
+  const data = querySnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+  // console.log("Query result:", data);
 
-    return {
-      id: snapshot.id,
-      ...snapshot.data(),
-    };
-  } catch (error) {
-    console.error("Error get data by ID:", error);
-    return null;
+  if (data.length === 0) {
+    // user belum ada -> boleh daftar
+    await addDoc(collection(db, "users"), userData);
+    // console.log("User registered:", userData);
+    callback({
+      status: "success",
+      message: "User registered successfully",
+    });
+  } else {
+    callback({
+      status: "error",
+      message: "User already exists",
+    });
   }
 }
--- P14/my-app/src/views/auth/register/index.tsx
+++ P14/my-app/src/views/auth/register/index.tsx
@@ -1,12 +1,46 @@
 import Link from "next/link";
 import style from "./register.module.scss";
+import { useState } from "react";
+import { useRouter } from "next/router"
 
 const TampilanRegister = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { push } = useRouter();
+  const [error, setError] = useState("");
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get("email") as string;
+    const fullname = formData.get("fullname") as string;
+    const password = formData.get("password") as string;
+    const response = await fetch("/api/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, fullname, password }),
+    });
+    // const result = await response.json();
+    // console.log(result);
+    if (response.status === 200) {
+      form.reset();
+      // event.currentTarget.reset();
+      setIsLoading(false);
+      push("/auth/login");
+    } else {
+      setIsLoading(false);
+      setError(
+        response.status === 400 ? "User already exists" : "An error occurred",
+      );
+    }
+  };
+  
     return (
         <div className={style.register}>
             <h1 className={style.register_title}>Halaman Register</h1>
             <div className={style.register_form}>
-                <form action="">
+                <form action="" onSubmit={handleSubmit}>
                     <div className={style.register_form_item}>
                         <label 
                         htmlFor="email" 
