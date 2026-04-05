eec0a2ae668c8bf993adbed47b1917f52954940f
vanssea <cristinvanessa6@gmail.com>
2026-04-05 14:57:33 +0700

    JS 17_Konfigurasi Google Provider di NextAuth dan Handle Callback JWT & Session

--- P16/my-app/src/pages/api/auth/[...nextauth].ts
+++ P16/my-app/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
+import GoogleProvider from "next-auth/providers/google"
 import bcrypt from "bcrypt"
 import { signIn } from "@/utils/db/servicefirebase"
 
@@ -37,7 +38,11 @@ export const authOptions:NextAuthOptions = {
         }
         return null
       }
-    })
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
   ],
 
   pages: {
@@ -51,6 +56,22 @@ export const authOptions:NextAuthOptions = {
         token.fullname = user.fullname
         token.role = user.role
       }
+
+      // Jika login dengan Google, tambahkan informasi yang diperlukan ke token
+      if (account?.provider === "google") {
+        const data = {
+          fullname: user.name,
+          email: user.email,
+          image: user.image,
+          type: account.provider,
+        }
+        // console.log("Google login data", { data })
+        token.fullname = data.fullname
+        token.email = data.email
+        token.image = data.image
+        token.type = data.type
+      }
+
       // console.log("jwt callback", { token, account, profile, user })
       return token
     },
@@ -61,8 +82,14 @@ export const authOptions:NextAuthOptions = {
       if (token.fullname) {
         session.user.fullname = token.fullname
       }
+      if (token.image) {
+        session.user.image = token.image
+      }
       if (token.role) {
         session.user.role = token.role
+      }
+      if (token.type) {
+        session.user.type = token.type
       }
       // console.log("session callback", { session, token })
       return session
