1af06ed691b43b7cfce5064a5f989d9a43249ecf
vanssea <cristinvanessa6@gmail.com>
2026-04-05 16:01:01 +0700

    JS 17_Simpan Data Google ke Database

--- P16/my-app/src/pages/api/auth/[...nextauth].ts
+++ P16/my-app/src/pages/api/auth/[...nextauth].ts
@@ -2,7 +2,7 @@ import NextAuth, { NextAuthOptions } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 import GoogleProvider from "next-auth/providers/google"
 import bcrypt from "bcrypt"
-import { signIn } from "@/utils/db/servicefirebase"
+import { signIn, signInWithGoogle } from "@/utils/db/servicefirebase"
 
 export const authOptions:NextAuthOptions = {
   session: {
@@ -65,11 +65,17 @@ export const authOptions:NextAuthOptions = {
           image: user.image,
           type: account.provider,
         }
-        // console.log("Google login data", { data })
-        token.fullname = data.fullname
-        token.email = data.email
-        token.image = data.image
-        token.type = data.type
+
+        await signInWithGoogle(data, (result: any) => {
+          // Pastikan mengecek result.status sesuai dengan object yang dikirim
+          if (result.status) {
+            token.fullname = result.data.fullname
+            token.email = result.data.email
+            token.image = result.data.image
+            token.type = result.data.type
+            token.role = result.data.role
+          }
+        })
       }
 
       // console.log("jwt callback", { token, account, profile, user })
--- P16/my-app/src/utils/db/servicefirebase.ts
+++ P16/my-app/src/utils/db/servicefirebase.ts
@@ -8,6 +8,7 @@ import {
   query,
   addDoc,
   where,
+  updateDoc,
 } from "firebase/firestore";
 import app from "./firebase";
 import bcrypt from "bcrypt";
@@ -88,6 +89,50 @@ export async function signUp(
     callback({
       status: "error",
       message: "User already exists",
+    });
+  }
+}
+
+export async function signInWithGoogle(
+  userData: any,
+  callback: any,
+) {
+  try {
+    const q = query(
+      collection(db, "users"),
+      where("email", "==", userData.email),
+    );
+
+    const querySnapshot = await getDocs(q);
+    const data: any[] = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+
+    if (data.length > 0) {
+      // User sudah ada, update data
+      userData.role = data[0].role;
+      await updateDoc(doc(db, "users", data[0].id), userData);
+      callback({
+        status: true,
+        message: "User registered and logged in with Google",
+        data: userData,
+      });
+    } else {
+      // User baru, tambah data
+      userData.role = "member";
+      await addDoc(collection(db, "users"), userData);
+      callback({
+        status: true,
+        message: "User registered and logged in with Google",
+        data: userData,
+      });
+    }
+  } catch (error: any) {
+    // Tangani error di sini
+    callback({
+      status: false,
+      message: "Failed to register user with Google",
     });
   }
 }
