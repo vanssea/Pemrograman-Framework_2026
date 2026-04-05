965c80361b6cb4bb74c3ad818ba7052250b57e84
vanssea <cristinvanessa6@gmail.com>
2026-04-05 20:05:04 +0700

    PRAKTIKUM 2 – Font Optimization Menggunakan next/font

--- P17/my-app/src/components/layouts/Appshell/index.tsx
+++ P17/my-app/src/components/layouts/Appshell/index.tsx
@@ -1,7 +1,13 @@
 import { useRouter } from 'next/router';
 import Navbar from '../navbar';
+import { Roboto } from 'next/font/google';
 
 const disableNavbar = ['/auth/login', '/auth/register', '/404'];
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+});
 
 type AppShellProps = {
   children: React.ReactNode;
@@ -12,7 +18,7 @@ const AppShell = (props: AppShellProps) => {
   const { pathname } = useRouter();
 
   return (
-    <main>
+    <main className={roboto.className}>
       {!disableNavbar.includes(pathname) && <Navbar />}
       {children}
     </main>
