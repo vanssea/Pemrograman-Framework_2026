4fbb728be61a1203cd1f157040c51ce41e3e31e3
vanssea <cristinvanessa6@gmail.com>
2026-04-05 19:24:19 +0700

    PRAKTIKUM 1 – Image Optimization

--- P17/my-app/src/pages/404.tsx
+++ P17/my-app/src/pages/404.tsx
@@ -1,35 +1,21 @@
-import Head from "next/head";
-import styles from "../styles/404.module.scss";
-import Link from "next/link";
+import Image from "next/image";
+import styles from "@/styles/404.module.scss";
 
-export default function Custom404() {
+const Custom404 = () => {
   return (
-    <>
-      <Head>
-        <title>404 - Halaman Tidak Ditemukan</title>
-        <meta
-          name="description"
-          content="Maaf, halaman yang kamu cari tidak tersedia."
-        />
-      </Head>
-
-      <div className={styles.error}>
-        <img
-          src="/page-not-found.png"
-          alt="404"
-          className={styles.error__image}
-        />
-
-        <h1 className={styles.error__title}>Oops! 404</h1>
-
-        <p className={styles.error__desc}>
-          Halaman yang kamu cari tidak ditemukan atau sudah dipindahkan.
-        </p>
-
-        <Link href="/" className={styles.error__button}>
-          Kembali ke Beranda
-        </Link>
-      </div>
-    </>
+    <div className={styles.error}>
+      {/* <img src="/page-not-found.png" alt="404" className={styles.error__image} /> */}
+      <Image
+        src="/page-not-found.png"
+        alt="404"
+        width={400}
+        height={200}
+        className={styles.error__image}
+      />
+      <h1>404 - Halaman Tidak Ditemukan</h1>
+      <p>Maaf, halaman yang Anda cari tidak ada.</p>
+    </div>
   );
-}
+};
+
+export default Custom404;
