83f2efc02e389503f787f277cf61ec173c3d2d9e
vanssea <cristinvanessa6@gmail.com>
2026-04-06 21:01:16 +0700

    PRAKTIKUM 5 – Konfigurasi Coverage Lengkap

--- P18/my-app/jest.config.mjs
+++ P18/my-app/jest.config.mjs
@@ -5,8 +5,21 @@ const createJestConfig = nextJest({
 })
 
 const config = {
-  coverageProvider: 'v8',
   testEnvironment: 'jsdom',
+  modulePaths: ['<rootDir>/src/'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/.next/**',
+    '!**/coverage/**',
+    '!**/jest.config.mjs',
+    '!**/next.config.mjs',
+    '!**/types/**',
+    '!**/views/**',
+    '!**/pages/api/**'
+  ],
 }
 
 export default createJestConfig(config)
