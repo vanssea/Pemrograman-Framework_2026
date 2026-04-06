5290fced6f759a625d2d43926e75982a3d7f5dad
vanssea <cristinvanessa6@gmail.com>
2026-04-06 21:22:52 +0700

    PRAKTIKUM 6 – Testing dengan getByTestId

--- P18/my-app/src/__test__/pages/about.spec.tsx
+++ P18/my-app/src/__test__/pages/about.spec.tsx
@@ -1,10 +1,11 @@
-import { render } from "@testing-library/react"
-import { describe, expect, it } from "@jest/globals"
+import { render, screen } from "@testing-library/react"
+import { describe, it, expect } from "@jest/globals"
 import AboutPage from "@/pages/about"
 
 describe("About Page", () => {
 	it("renders about page correctly", () => {
-		const page = render(<AboutPage />)
-		expect(page).toMatchSnapshot()
+		render(<AboutPage />)
+		expect(screen.getByTestId("title").textContent).toBe("About Page")
+		expect(screen.getByTestId("title")).toMatchSnapshot()
 	})
 })
--- P18/my-app/src/pages/about/index.tsx
+++ P18/my-app/src/pages/about/index.tsx
@@ -1,45 +1,12 @@
 import Head from "next/head";
 import Link from "next/link";
 
-const mainStyle = {
-  padding: "40px",
-  fontFamily: "Arial",
-};
-
-const biodataStyle = {
-  marginTop: "20px",
-};
-
-const backLinkWrapperStyle = {
-  marginTop: "30px",
-};
-
-export default function About() {
+const AboutPage = () => {
   return (
-    <>
-      <Head>
-        <title>About | Next.js App</title>
-      </Head>
-
-      <main style={mainStyle}>
-        <h1>Halaman About</h1>
-
-        <div style={biodataStyle}>
-          <p>
-            <strong>Nama Mahasiswa:</strong> Vanessa Cristin Natalia
-          </p>
-          <p>
-            <strong>NIM:</strong> 2341720026
-          </p>
-          <p>
-            <strong>Program Studi:</strong> D4 Teknik Informatika
-          </p>
-        </div>
-
-        <div style={backLinkWrapperStyle}>
-          <Link href="/">← Kembali ke Halaman Utama</Link>
-        </div>
-      </main>
-    </>
+    <div>
+      <h1 data-testid="title">About Page</h1>
+    </div>
   );
-}
+};
+
+export default AboutPage;
